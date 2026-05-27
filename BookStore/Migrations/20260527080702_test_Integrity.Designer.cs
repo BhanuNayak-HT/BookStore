da341e7649fe0355aa4c28c256f540a490c55b2e
Bhanu Nayak <bhanun@heaptrace.com>
2026-05-27 13:42:30 +0530

    [feat]: added normal migration to check action run

--- BookStore/Migrations/20260527080702_test_Integrity.Designer.cs
+++ BookStore/Migrations/20260527080702_test_Integrity.Designer.cs
@@ -4,6 +4,7 @@ using BookStore.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookStore.Migrations
 {
     [DbContext(typeof(BookStoreDBContext))]
-    partial class BookStoreDBContextModelSnapshot : ModelSnapshot
+    [Migration("20260527080702_test_Integrity")]
+    partial class test_Integrity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
